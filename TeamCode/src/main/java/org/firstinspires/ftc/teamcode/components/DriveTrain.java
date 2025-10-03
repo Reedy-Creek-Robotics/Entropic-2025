cd55354dd0e5135732939b8177b44185a293bc5b
Cocoiscool-531 <119710842+Cocoiscool-531@users.noreply.github.com>
2025-10-02 23:45:34 -0400

    TeleOp, shootAtDistance(), getters in Robot, driver centric method, otos in DriveTrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/DriveTrain.java
@@ -5,22 +5,30 @@ import static org.firstinspires.ftc.teamcode.components.RobotDescriptor.Odometry
 
 import android.annotation.SuppressLint;
 
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.geometry.Heading;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.teamcode.util.DriveUtil;
 
 import java.util.Arrays;
 import java.util.List;
+
+//ToDo Create method to turn n degrees
 
 @SuppressLint("DefaultLocale")
 public class DriveTrain extends BaseComponent {
     @Override
     public void update() {
     }
+
+    SparkFunOTOS otos;
 
     private DcMotorEx leftFront, leftRear, rightRear, rightFront;
     private List<DcMotorEx> motors;
@@ -32,19 +40,13 @@ public class DriveTrain extends BaseComponent {
 
     public DriveTrain(RobotContext context) {
         super(context);
+    }
+
+    @Override
+    public void init() {
 
         driveTuner = descriptor.DRIVE_TUNER;
         odometryTuner = descriptor.ODOMETRY_TUNER;
-
-        //this.context.localizer = new StandardTrackingWheelLocalizer(hardwareMap, lastTrackingEncPositions, lastTrackingEncVels, odometryTuner);
-
-        // TODO: adjust the names of the following hardware devices to match your configuration
-        /*
-        imu = hardwareMap.get(IMU.class, "imu");
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                DriveConstants.LOGO_FACING_DIR, DriveConstants.USB_FACING_DIR));
-        imu.initialize(parameters);
-        */
 
         leftFront = hardwareMap.get(DcMotorEx.class, "lf");
         leftRear = hardwareMap.get(DcMotorEx.class, "lr");
@@ -74,8 +76,25 @@ public class DriveTrain extends BaseComponent {
         if (driveTuner.runUsingEncoder && driveTuner.driveMotorVeloPid != null) {
             setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, driveTuner.driveMotorVeloPid);
         }
+
+        otos = hardwareMap.get(SparkFunOTOS.class, "otos");
+
+        otos.setLinearScalar(Constants.otosLocalizerConstants.linearScalar);
+        otos.setAngularScalar(Constants.otosLocalizerConstants.angularScalar);
+
+        otos.setOffset(new SparkFunOTOS.Pose2D(
+                0,
+                0,
+                0
+        ));
+
+        otos.calibrateImu();
+        otos.resetTracking();
     }
 
+    public SparkFunOTOS getOtos(){
+        return otos;
+    }
 
     public void setMode(DcMotor.RunMode runMode) {
         for (DcMotorEx motor : motors) {
@@ -109,8 +128,40 @@ public class DriveTrain extends BaseComponent {
         rightRear.setPower(motorPowers.backRight);
     }
 
-    public void driverRelative(double drive, double strafe, double turn) {
+    public void driverRelative(double drive, double strafe, double turn, double speedFactor) {
+        DriveUtil.MotorPowers motorPowers = context.driveUtil.calculateWheelPowerForDriverRelative(drive, strafe, turn, new Heading(otos.getPosition().h), speedFactor);
 
+        // DriveUtil.MotorPowers motorPowers = context.driveUtil.calculateWheelPowerForDriverRelative(drive, strafe, turn, new Heading(), speedFactor);
+
+        leftFront.setPower(motorPowers.frontLeft);
+        leftRear.setPower(motorPowers.backLeft);
+        rightFront.setPower(motorPowers.frontRight);
+        rightRear.setPower(motorPowers.backRight);
+    }
+
+    public void calibrateOtos(){
+        executeCommand(new CalibrateOtos());
+    }
+
+    public class CalibrateOtos implements Command{
+
+        ElapsedTime timer;
+
+        @Override
+        public void start() {
+            timer = new ElapsedTime();
+            otos.calibrateImu(255, false);
+        }
+
+        @Override
+        public void stop() {
+
+        }
+
+        @Override
+        public boolean update() {
+            return timer.milliseconds() > 750;
+        }
     }
 
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpMain.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.components.Robot;
+import org.firstinspires.ftc.teamcode.game.Controller;
+
+@TeleOp
+public class TeleOpMain extends OpMode {
+
+    Controller.Button TRANSFER_SHOOT = Controller.Button.Y;
+    Controller.Button INTAKE = Controller.Button.A;
+
+    Robot robot;
+
+    protected Controller driver;
+
+    private double drive = 0, turn = 0, strafe = 0, speedFactor = 1;
+
+    boolean intaking;
+
+    @Override
+    public void init() {
+        robot = new Robot(this, 1); //Red Alliance
+        driver = new Controller(gamepad1);
+        robot.getDriveTrain().calibrateOtos();
+    }
+
+    @Override
+    public void loop() {
+        driveDriver();
+
+        if(driver.isPressed(TRANSFER_SHOOT)){
+            robot.getShooter1().shootAtDistance(robot.getAprilTag().getFtcPose(24).range);
+            robot.getTransfer1().transferUntilShot();
+        }
+
+        if(driver.isPressed(INTAKE)){
+            intaking = !intaking;
+            robot.getIntake().driveIntake(intaking ? 1 : 0);
+        }
+
+        if(driver.isPressed(Controller.Button.DPAD_UP)){
+            speedFactor += 0.1;
+        } else if (driver.isPressed(Controller.Button.DPAD_DOWN)) {
+            speedFactor -= 0.1;
+        }
+
+    }
+
+    /**
+     * Robot centric drive method
+     */
+    private void driveBot(){
+        drive = driver.leftStickY();
+        strafe = driver.leftStickX();
+        turn = driver.rightStickX();
+
+        robot.getDriveTrain().drive(drive, strafe, turn, speedFactor);
+    }
+
+    /**
+     * Driver centric drive method
+     */
+    private void driveDriver(){
+        drive = driver.leftStickY();
+        strafe = driver.leftStickX();
+        turn = driver.rightStickX();
+
+        robot.getDriveTrain().driverRelative(drive,strafe,turn,1);
+    }
+}
