a3c454cc50a7c8e669960705a690375d07ba516b
Cocoiscool-531 <119710842+Cocoiscool-531@users.noreply.github.com>
2025-11-21 20:20:21 -0500

    i dont know

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ShooterLogger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ShooterLogger.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -9,6 +10,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 import org.firstinspires.ftc.teamcode.game.Controller;
 import org.firstinspires.ftc.teamcode.util.log.DataLog;
@@ -16,6 +18,7 @@ import org.firstinspires.ftc.teamcode.util.log.DataLog;
 import java.text.SimpleDateFormat;
 
 @TeleOp
+@Configurable
 public class ShooterLogger extends OpMode {
     TelemetryManager telem;
 
@@ -28,10 +31,12 @@ public class ShooterLogger extends OpMode {
     VoltageSensor volt;
     int identifier = 0;
 
-    static int graphMin = 0;
-    static int graphMax = 0;
+    static double graphMin = 0;
+    double graphMax = 0;
 
-    double velocity = 2000;
+    static double velocity = 2000;
+
+    static boolean logging = true;
 
     @Override
     public void init() {
@@ -52,6 +57,8 @@ public class ShooterLogger extends OpMode {
 
         roller1.setPosition(1);
         roller2.setPosition(-1);
+
+        telemetry.setDisplayFormat(Telemetry.DisplayFormat.MONOSPACE);
     }
 
     @Override
@@ -80,34 +87,49 @@ public class ShooterLogger extends OpMode {
             identifier--;
         }
 
-        if(controller.isPressed(Controller.Button.CIRCLE)){
+        if(controller.isPressed(Controller.Button.EAST)){
             velocity = 0;
         }
 
-        if(controller.isPressed(Controller.Button.CROSS)){
-            velocity = 2000;
+        if(controller.isPressed(Controller.Button.SOUTH)){
+            velocity = velocity;
+        }
+
+        if(controller.isPressed(Controller.Button.NORTH)){
+            logging = !logging;
+        }
+
+        if(graphMax - 100 < shooter.getVelocity()){
+            graphMax = shooter.getVelocity() + 100;
         }
 
         shooter.setVelocity(velocity);
         
-        telem.addLine("IDENTIFIER:");
-        telem.addLine("1 - NO WEIGHT | 2 - LIGHT WEIGHT");
-        telem.addLine("3 - HEAVY WEIGHT | 2 - BOTH WEIGHT");
-        telem.addLine("");
+        telem.addLine("CHANGE IDENTIFIER WITH BUMPERS:");
+        telem.addLine("1 - NO WEIGHT    | 2 - LIGHT WEIGHT");
+        telem.addLine("3 - HEAVY WEIGHT | 4 - BOTH WEIGHT");
+        telem.addLine("Press EAST to set velocity to 0");
+        telem.addLine("Press SOUTH to set velocity to 2000");
+        telem.addLine("-- Press NORTH to start logging --");
+        telem.addData("Logging", logging);
         telem.addData("Identifier", identifier);
         telem.addData("Set Vel", velocity);
         telem.addData("Real Vel", shooter.getVelocity());
-        telem.addData("Graph Min", graphMin);
-        telem.addData("Graph Max", graphMax);
+        // These are flat lines to keep the graph scale consistent
+        telem.addData("Graph Min", graphMin); // graph min will always be 0
+        telem.addData("Graph Max", graphMax); // graph max will be set to the highest velocity recorded + 100
         telem.update();
 
-        dataLog.identifier.set(identifier);
-        dataLog.current.set(shooter.getCurrent(CurrentUnit.AMPS));
-        dataLog.ticks.set(shooter.getCurrentPosition());
-        dataLog.setVelocity.set(velocity);
-        dataLog.velocity.set(shooter.getVelocity());
-        dataLog.motorType.set(shooter.getMotorType().getDescription());
-        dataLog.batteryVoltage.set(volt.getVoltage());
-        dataLog.writeLine();
+
+        if (logging) {
+            dataLog.identifier.set(identifier);
+            dataLog.current.set(shooter.getCurrent(CurrentUnit.AMPS));
+            dataLog.ticks.set(shooter.getCurrentPosition());
+            dataLog.setVelocity.set(velocity);
+            dataLog.velocity.set(shooter.getVelocity());
+            dataLog.motorType.set(shooter.getMotorType().getDescription());
+            dataLog.batteryVoltage.set(volt.getVoltage());
+            dataLog.writeLine();
+        }
     }
 }
