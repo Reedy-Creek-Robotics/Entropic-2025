7c233b8be7464f84cddf0a404630ed8c78499e69
Lockeshor <mrdoodly111@gmail.com>
2025-12-11 14:04:45 -0500

    BMS outlined

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Transtake.java
@@ -0,0 +1,150 @@
+package org.firstinspires.ftc.teamcode.components;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class Transtake extends BaseComponent {
+
+    //0 = no balls in robot
+    //1 = one ball in center
+    //2 = one ball in center, one ball in front
+    //3 = one ball in center, one ball in back
+    //4 = three balls in robot
+    int ballState;
+
+    private DcMotorEx intakeFront;
+    private DcMotorEx intakeBack;
+    private CRServo rollerFront;
+    private CRServo rollerBack;
+
+    public Transtake(RobotContext context) {
+        super(context);
+    }
+
+    @Override
+    public void init() {
+        intakeFront = hardwareMap.get(DcMotorEx.class, "intakeFront");
+        intakeBack = hardwareMap.get(DcMotorEx.class, "intakeBack");
+        rollerFront = hardwareMap.get(CRServo.class, "rollerFront");
+        rollerBack = hardwareMap.get(CRServo.class, "rollerBack");
+
+        //change depending on auto - may need to grab from file
+        ballState = 0;
+    }
+
+    @Override
+    public void update(){
+        telemetry.addData("ballState",ballState);
+    }
+
+    public int getBallState() {
+        return ballState;
+    }
+
+    public void setBallState(int ballState) {
+        this.ballState = ballState;
+    }
+
+//BMS PLAN
+
+//first ball enters from either side
+//  put in center position
+//second ball enters from either side
+//  first ball is moved to opposite side
+//  second ball is moved to the center
+//third ball enters
+//  if on same side as second ball
+//      put on side it entered in
+//  otherwise
+//      push first ball from center to opposite side of entry
+//      push second ball from entry side to center
+//      put on side it entered in
+
+    public void incomingFront() {
+        switch (ballState){
+            case 0: //no ball in yet
+
+                //run rollerFront in for 2 seconds
+                executeCommand(new RollerForTime(rollerFront, 1, 2000));
+                break;
+
+            case 1: //ball in center
+
+                //run rollerBack out for 2 seconds
+                executeCommand(new RollerForTime(rollerBack, -1, 2000));
+                //run rollerFront in for 2 seconds
+                executeCommand(new RollerForTime(rollerFront, 1, 2000));
+                break;
+
+            case 2: //balls in center & front
+
+                //run rollerFront in AND rollerBack out for 3 seconds
+                executeCommand(new RollerForTime(rollerFront, 1, 3000));
+                executeCommand(new RollerForTime(rollerBack, -1, 3000));
+                break;
+
+            case 3: //balls in center & back
+
+                //run rollerFront in for 2 seconds
+                executeCommand(new RollerForTime(rollerFront, 1, 2000));
+                break;
+        }
+    }
+
+    public void incomingBack() {
+        switch(ballState){
+            case 0: //no ball in yet
+
+                //run rollerBack in for 2 seconds
+                executeCommand(new RollerForTime(rollerBack, 1, 2000));
+                break;
+
+            case 1: //ball in center
+
+                //run rollerFront out for 2 seconds
+                executeCommand(new RollerForTime(rollerFront, -1, 2000));
+                //run rollerBack in for 2 seconds
+                executeCommand(new RollerForTime(rollerBack, 1, 2000));
+                break;
+
+            case 2: //balls in center & front
+
+                break;
+
+            case 3: //balls in center & back
+
+                break;
+        }
+    }
+
+    public class RollerForTime implements Command {
+
+        CRServo roller;
+        double power, time;
+        ElapsedTime timer;
+
+        public RollerForTime(CRServo roller, double power, double timeMs){
+            this.roller = roller;
+            this.power = power;
+            this.time = timeMs;
+        }
+
+        @Override
+        public void start(){
+            roller.setPower(power);
+            timer = new ElapsedTime();
+        }
+
+        @Override
+        public void stop() {
+            roller.setPower(0);
+        }
+
+        @Override
+        public boolean update() {
+            return timer.milliseconds() > time;
+        }
+    }
+}
