854b5fc4edce8d179e716ec8d62adb639042bd73
Cocoiscool-531 <119710842+Cocoiscool-531@users.noreply.github.com>
2026-01-05 15:54:53 -0500

    Start test auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/TestAuto.java
@@ -0,0 +1,18 @@
+/*package org.firstinspires.ftc.teamcode.pedro; // make sure this aligns with class location
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous(name = "Example Auto", group = "Examples")
+public class TestAuto extends OpMode {
+
+    private Follower follower;
+    private Timer pathTimer, actionTimer, opmodeTimer;
+
+    private int pathState;*/
