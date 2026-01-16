f7f7913acaa2bc186b8b848bd73713a593992849
Cocoiscool-531 <119710842+Cocoiscool-531@users.noreply.github.com>
2026-01-15 19:14:57 -0500

    code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RedCobySingle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RedCobySingle.java
@@ -4,13 +4,14 @@ import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 
-public class BlueCobySingle extends CobySingle{
+public class RedCobySingle extends CobySingle {
+
     @Override
     public void buildPaths() {
         paths = new Paths(follower);
     }
 
-    public static class Paths extends CobySingle.Paths{
+    public static class Paths extends CobySingle.Paths {
 
         public Paths(Follower follower) {
             super(follower);
@@ -20,46 +21,42 @@ public class BlueCobySingle extends CobySingle{
         public void createPaths(Follower follower) {
             shootPreload = follower.pathBuilder().addPath(
                             new BezierLine(
-                                    new Pose(54.000, 6.000),
-                                    new Pose(60.000, 12.000)
+                                    new Pose(90.000, 6.000),
+                                    new Pose(84.000, 12.000)
                             )
                     ).setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(270))
                     .build();
 
             alignWithBallSet3 = follower.pathBuilder().addPath(
                             new BezierLine(
-                                    new Pose(60.000, 12.000),
-
-                                    new Pose(42.000, 36.000)
+                                    new Pose(84.000, 12.000),
+                                    new Pose(102.000, 36.000)
                             )
-                    ).setLinearHeadingInterpolation(Math.toRadians(270), Math.toRadians(180))
+                    ).setLinearHeadingInterpolation(Math.toRadians(270), Math.toRadians(0))
                     .build();
 
             pickupBallSet3 = follower.pathBuilder().addPath(
                             new BezierLine(
-                                    new Pose(42.000, 36.000),
-
-                                    new Pose(22.000, 36.000)
+                                    new Pose(102.000, 36.000),
+                                    new Pose(122.000, 36.000)
                             )
-                    ).setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(180))
+                    ).setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
                     .build();
 
             shootBallSet3 = follower.pathBuilder().addPath(
                             new BezierLine(
-                                    new Pose(22.000, 36.000),
-
-                                    new Pose(60.000, 12.000)
+                                    new Pose(122.000, 36.000),
+                                    new Pose(84.000, 12.000)
                             )
-                    ).setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(180))
+                    ).setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
                     .build();
 
             parking = follower.pathBuilder().addPath(
                             new BezierLine(
-                                    new Pose(60.000, 12.000),
-
-                                    new Pose(36.000, 12.000)
+                                    new Pose(84.000, 12.000),
+                                    new Pose(108.000, 12.000)
                             )
-                    ).setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(90))
+                    ).setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(90))
                     .build();
         }
     }
